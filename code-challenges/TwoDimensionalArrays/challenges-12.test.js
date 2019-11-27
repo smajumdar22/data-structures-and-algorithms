86fbd3b3f1fe19a08f90251fb87b038628a2f1a7
Shubham Majumdar <smajumdar22@gmail.com>
2019-11-26 21:14:35 -0800

    First 2 tests

--- code-challenges/TwoDimensionalArrays/challenges-12.test.js
+++ code-challenges/TwoDimensionalArrays/challenges-12.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { valueToNode } from "@babel/types";
+import { getConsoleOutput } from "@jest/console";
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1
@@ -51,8 +52,12 @@ Write a function named salesData that uses forEach to iterate over the hourlySal
 
 const salesData = (hours, data) => {
   // Solution code here...
-};
+  
+  hours.map((hour,idx)=> {
+    return {sales: `${data[idx]} cookies`, time: hour};
 
+  }
+  )};
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
