577e30205e8af2e50a7d05ae80fb107e55ea2643
smajum dar22 <smajumdar22@gmail.com>
2020-01-23 23:52:00 -0800

    Challenge 8

---
+++ code-challenges/linkedList/llkthfromend/ll-kth-from-end.js
@@ -0,0 +1,18 @@
+
+
+class Node{
+    constructor(element){
+      this.value = element;
+      this.next = null;
+    }
+  }
+  
+    class LinkedList{
+      constructor(){
+        this.head = null;
+      }
+    }
+  
+    llKthFromEnd(element){
+      
+    }
