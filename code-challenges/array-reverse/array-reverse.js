d8e55091d35b1186b62128b8dd64631e906bbda5
smajum dar22 <smajumdar22@gmail.com>
2020-03-16 22:44:44 -0700

    Refactor

--- code-challenges/array-reverse/array-reverse.js
+++ code-challenges/array-reverse/array-reverse.js
@@ -2,18 +2,9 @@
 
 const reverseArray = (arr) => {
 
-let newArr = [];
-for (var i = 0, j = arr.length - 1; i < arr.length; i++, j--) {      
-    newArr[i] = arr[j];
-}   
-return newArr;
+    let newArr = [];
+    for (var i = 0, j = arr.length - 1; i < arr.length; i++, j--) {
+        newArr[i] = arr[j];
+    }
+    return newArr;
 };
-
-//______________________________________________________________________
-
-describe('Testing challenge 1', () => {
-    test('It should reverse the array', () => {
-      const arr = ["A", "B", "C", "D", "E", "F"];
-      expect(reverseArray(arr)).toEqual( ["F", "E", "D", "C", "B", "A"]);
-    });
-  });
