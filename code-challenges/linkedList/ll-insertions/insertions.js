c6c608834405502b6e05a2e6dcd1aaf448bcaf74
smajum dar22 <smajumdar22@gmail.com>
2020-04-07 16:47:30 -0700

    Added class demo

--- code-challenges/linkedList/ll-insertions/insertions.js
+++ code-challenges/linkedList/ll-insertions/insertions.js
@@ -4,6 +4,7 @@ class Node {
   constructor(value){
     this.value = value
     this.next = null;
+    this.prev = null;
   }
 }
 
@@ -65,47 +66,99 @@ class LinkedList {
     this.size++
   }
 
+  insertBefore(newVal, beforeVal){
+    let previousCurrentNode = null;
+    let currentNode = this.head;
 
-  insertBefore(value, newVal) {
-    if (value === undefined || newVal === undefined) {
-        throw Error('Invalid input');
+    if(currentNode && currentNode.val === beforeVal){
+      //we are looking for the head
+      let newNode = new Node(newVal);
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
     }
-    let node = new Node(newVal);
-    let current = this.head;
-    while (current.next) {
-        if (this.head.data === value) {
-            node.next = this.head;
-            this.head = node;
-        } else if (current.next.data === value) {
-            node.next = current.next;
-            current.next = node;
-        } else {
-            current = current.next;
-        }
+
+    while(currentNode){
+          if(currentNode.val === beforeVal){
+            let newNode = new Node(newVal);
+            previousCurrentNode.next = newNode;
+            newNode.next = currentNode;
+            return;
+            //found my val
+            //check val.next is same
+
+          }
+
+          previousCurrentNode = currentNode;
+          currentNode = currentNode.next;
     }
-    return this;
+
+    return false;
+
+  }
+
+//   insertBefore(value, newVal) {
+//     if (value === undefined || newVal === undefined) {
+//         throw Error('Invalid input');
+//     }
+//     let node = new Node(newVal);
+//     let current = this.head;
+//     while (current.next) {
+//         if (this.head.data === value) {
+//             node.next = this.head;
+//             this.head = node;
+//         } else if (current.next.data === value) {
+//             node.next = current.next;
+//             current.next = node;
+//         } else {
+//             current = current.next;
+//         }
+//     }
+//     return this;
+// }
+
+insertAfter(newVal, afterVal){
+  let currentNode = this.head;
+  // if(currentNode.val === afterVal){
+  //   let newNode = new Node(newVal);
+  //   this.head.next = newNode;
+  //   return;
+  // }
+
+  while(currentNode){
+    if(currentNode.val === afterVal){
+      let newNode = new Node(newVal);
+      let nextCurrentNode = currentNode.next;
+      currentNode.next = newNode;
+      newNode.next = nextCurrentNode;
+
+    }
+    currentNode = currentNode.next;
+  }
+
+  return false;
+
 }
-
-insertAfter(value, newValue) {
-    if (value === undefined || newValue === undefined) {
-        throw Error('Invalid Input');
-    }
-    let node = new Node(newValue);
-    let current = this.head;
+// insertAfter(value, newValue) {
+//     if (valuecurrentNode.next === undefined || newValue === undefined) {
+//         throw Error('Invalid Input');
+//     }
+//     let node = new Node(newValue);
+//     let current = this.head;
    
-    while (current.next) {
-        if (this.head.data === value) {
-            node.next = this.head.next;
-            this.head.next = node;
-        } else if (current.next.data === value) {
-            node.next = current.next.next;
-            current.next.next = node;
-        } else {
-            current = current.next;
-        }
-    }
-    return this;
-}
+//     while (current.next) {
+//         if (this.head.data === value) {
+//             node.next = this.head.next;
+//             this.head.next = node;
+//         } else if (current.next.data === value) {
+//             node.next = current.next.next;
+//             current.next.next = node;
+//         } else {
+//             current = current.next;
+//         }
+//     }
+//     return this;
+// }
 
 }
 
