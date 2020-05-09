1e369b2c4c0efdebf0b4060771e831c2b843a995
smajum dar22 <smajumdar22@gmail.com>
2020-05-09 11:54:40 -0700

    Refactor

--- code-challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ code-challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -1,33 +1,56 @@
 'use strict';
 
-function fizzBuzzTree(node) {
+const Node = require('./node.js');
 
-  if (!node) return 'Must be a valid value';
-  if (typeof node.root.value !== 'number') return 'Need to be a number'
+const fizzBuzzTree =(root)=> {
+  let replacementStr ='';
+  if(root.val %3 === 0 && root.val %5 === 0 ) replacementStr += 'FizzBuzz';
+  if(root.val %3 === 0) replacementStr += 'Fizz';
+  if(root.val %5 === 0) replacementStr += 'Buzz';
 
-  if (typeof node.root.value === 'number') {
+  let newRoot = new Node(replacementStr ? replacementStr : `${root.val}`);
 
-    if (node.root.value % 3 === 0 && node.root.value % 5 === 0) {
-      node.root.value = 'FizzBuzz';
-    } else if (node.root.value % 3 === 0) {
-      node.root.value = 'Fizz';
-    } else if (node.root.value % 5 === 0) {
-      node.root.value = 'Buzz';
-    }else{
-      node.root.value = node.root.value.toString();
-    }
+    if (root.left) {
+
+    fizzBuzzTree(root.left);
+  }
+  if (root.right) {
+
+    fizzBuzzTree(root.right);
   }
 
-  if (node.left) {
+  return newRoot;
+}
 
-    fizzBuzzTree(node.left);
-  }
-  if (node.right) {
+// function fizzBuzzTree(node) {
 
-    fizzBuzzTree(node.right);
-  }
-  return node;
-};
+//   if (!node) return 'Must be a valid value';
+//   if (typeof node.root.value !== 'number') return 'Need to be a number'
+
+//  let replacementStr = '';
+//   if (typeof node.root.value === 'number') {
+
+//     if (node.val % 3 === 0 && node.val % 5 === 0) {
+//       node.val = 'FizzBuzz';
+//     } else if (node.val % 3 === 0) {
+//       node.val = 'Fizz';
+//     } else if (node.val % 5 === 0) {
+//       node.val = 'Buzz';
+//     }else{
+//       node.val = replacementStr;
+//     }
+//   }
+
+//   if (node.left) {
+
+//     fizzBuzzTree(node.left);
+//   }
+//   if (node.right) {
+
+//     fizzBuzzTree(node.right);
+//   }
+//   return new Node(node);
+// };
 
 
 
