2c5cb65465122e6c647e85cda568428042a9789c
Shubham <smajumdar22@gmail.com>
2020-06-27 15:08:56 -0700

    Implementation of repeated word with hash

--- code-challenges/repeatWord/__tests__/repeatedword.test.js
+++ code-challenges/repeatWord/__tests__/repeatedword.test.js
@@ -12,7 +12,7 @@ describe('Test repeated words in a String',() =>{
     it('can test no repeated word',()=>{
         let tesStr = 'ab ac ad ae';
 
-        expect(repeatedWord(tesStr)).toEqual('None');
+        expect(repeatedWord(tesStr)).toEqual(null);
     })
 
     
@@ -23,8 +23,8 @@ describe('Test repeated words in a String',() =>{
     })
     
     it('can find repeated words',()=>{
-        let tesStr = 'ab ac ad ac ba';
+        let tesStr = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.....';
 
-        expect(repeatedWord(tesStr)).toEqual('ac');
+        expect(repeatedWord(tesStr)).toEqual('it');
     })
 })
