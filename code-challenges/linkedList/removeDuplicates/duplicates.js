89fe24b75fefe3a05529b638052e95bd16cb6513
smajumdar22 <smajumdar22@gmail.com>
2020-08-19 22:04:28 -0700

    Update comments

--- code-challenges/linkedList/removeDuplicates/duplicates.js
+++ code-challenges/linkedList/removeDuplicates/duplicates.js
@@ -42,9 +42,7 @@ class Node {
   }
 function remDuplicates(head) {
 
-    if(!head){
-        return 'Invalid'
-    }
+    if(!head) return ;
     const set = new Set();
     const hash = {};
     hash.next = head;
@@ -69,19 +67,19 @@ ll.next = new Node(2);
 ll.next.next = new Node(4);
 ll.next.next.next = new Node(2);
 
-//console.log(remDuplicates(ll)); //1 2 4
+console.log(remDuplicates(ll)); //1 2 4
 
 // Input - 1 1
 //Output - 1
 let test = new Node(1);
 test.next = new Node(1);
 
-//console.log(remDuplicates(test)); //1 
+console.log(remDuplicates(test)); //1 
 
 // Input - Null
-//Output - Invalid
+//Output - Undefined
 let test2 = new Node();
-//console.log(remDuplicates(test2)); //Invalid
+console.log(remDuplicates(test2)); //Undefined
 
 // Input - 1
 //Output - 1
